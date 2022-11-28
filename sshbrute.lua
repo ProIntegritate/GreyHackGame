15129c8aba5f454af7141bbe7ffb614ea0c3bfb7
ProIntegritate <54767353+ProIntegritate@users.noreply.github.com>
2022-11-29 00:25:47 +0100

    Update sshbrute.lua

--- sshbrute.lua
+++ sshbrute.lua
@@ -1,5 +1,9 @@
+
 // SSH Bruteforcer, Written by Ichinin
-if params.len == 0 or params[0] == "-h" or params[0] == "--help" then exit("<b>Usage: "+program_path.split("/")[-1]+" [passwordfile] [host] [username]</b>")
+if params.len == 0 or params[0] == "-h" or params[0] == "--help" then 
+	print ("<b>SSHBrute, written by Ichinin.</b>")
+	exit("<i>Usage: "+program_path.split("/")[-1]+" [passwordfile] [host] [username]</i>")
+end if
 
 passfile = params[0]
 host = params[1]
@@ -14,10 +18,19 @@ mypc = get_shell
 counter = 1
 
 for pw in passwords
-	print ("Testing password: " + pw + "  [" + counter + "/" + passwords.len + "]")
-	ms = mypc.connect_service(host,22,login,pw)
-	if ms then exit("Password for user " + login + " is " + pw)
-	counter = counter + 1
+	if pw != "" then
+		print ("Testing password: " + pw + "  [" + counter + "/" + passwords.len + "]")
+		
+		ms = mypc.connect_service(host,22,login,pw)
+		if not ms then 
+			pw = slice(pw, 0, 1).upper + slice(pw, 1, pw.len)
+			ms = mypc.connect_service(host,22,login,pw)
+		end if
+		// todo add for-next loop with trailing 0-9
+	
+		if ms then exit("Password for user " + login + " is " + pw)
+		counter = counter + 1
+		end if
 end for
 
 print ("Sorry, password not found")
