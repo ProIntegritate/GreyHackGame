449bc5895604ad1353697a65af86ef9d967c517f
ProIntegritate <54767353+ProIntegritate@users.noreply.github.com>
2022-11-29 15:53:16 +0100

    Update ver.lua

--- ver.lua
+++ ver.lua
@@ -1,7 +1,7 @@
 // ver.src, Get version of file. Enter full path to lib file
 // Written in 2022, Ichinin
 
-print ("Usage: ver <!!FULL!! path to /lib/file.so>")
+if params.len == 0 or params[0] == "-h" or params[0] == "--help" then exit("<b>Usage: "+program_path.split("/")[-1]+" [full path to lib]</b>")
 
 metaxploit = include_lib("/lib/metaxploit.so")
 if not metaxploit then
