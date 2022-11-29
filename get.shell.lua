45b674539a05947adccb17b838331dadd78de996
ProIntegritate <54767353+ProIntegritate@users.noreply.github.com>
2022-11-29 16:32:52 +0100

    Update get.shell.lua

--- get.shell.lua
+++ get.shell.lua
@@ -13,16 +13,9 @@ metaLib = net_session.dump_lib
 // 
 // Exploit chain: Guest accounts last resort for each service type.
 // 
-result = metaLib.overflow("0x5D51D0FD", "magetelitext") // libHTTP 1.0.2
-if not result then result = metaLib.overflow("0x121C868A", "codecodeintexttex") // libHTTP 1.0.1
-if not result then result = metaLib.overflow("0x74E6A816", "toredp") // libHTTP 1.0.3
-if not result then result = metaLib.overflow("0x314959AC", "aorb") // libHTTP 1.0.0 (guest)
-if not result then result = metaLib.overflow("0x481D8767", "mask") // libSMTP 1.0.0
-if not result then result = metaLib.overflow("0x14316037", "orsdistend_color_") // libFTP 1.0.0 (Guest)
-if not result then result = metaLib.overflow("0x61D11589", "ewporttextslc") // libSSH 1.0.3
-if not result then result = metaLib.overflow("0x56EB4E37", "intlinesizefirecttr") // libSSH 1.0.3
-if not result then result = metaLib.overflow("0x56EB4E37", "_flushpara") // libSSH 1.0.3 (guest)
-if not result then result = metaLib.overflow("0x31B2E17F", "timeanimpla") // libSSH 1.0.4 (guest)
+result = metaLib.overflow("0xoffset", "function") // libxxx 1.0.0
+if not result then result = metaLib.overflow("0xoffset", "function") // libxxx 1.0.0
+
 
 if not result then exit("Program ended")
 if typeof(result) == "shell" then
