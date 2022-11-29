a99987c5de46e78c92a7f813a1954f3038954d2e
ProIntegritate <54767353+ProIntegritate@users.noreply.github.com>
2022-11-29 16:42:25 +0100

    Update local.get.shell.lua

--- local.get.shell.lua
+++ local.get.shell.lua
@@ -9,15 +9,8 @@ if not metaLib then exit("Can't find " + "/lib/kernel_module.so")
 // 
 // Exploit chain
 // 
-result = metaLib.overflow("0x1302850E", "lusb")
-if not result then result = metaLib.overflow("0x5A9D2353", "orlinentainenumobjectev")
-if not result then result = metaLib.overflow("0x2F15DD42", "contextree")
-if not result then result = metaLib.overflow("0x40B4A387", "stored_bitsdyn_dt")
-if not result then result = metaLib.overflow("0x1302850E", "lusb")
-if not result then result = metaLib.overflow("0x6DD0D081", "note")
-if not result then result = metaLib.overflow("0x7B9BE70A", "bofitext")
-if not result then result = metaLib.overflow("0x75E941FB", "viewporttextefferro")
-if not result then result = metaLib.overflow("0x7B9BE70A", "greattim")
+result = metaLib.overflow("0xoffset", "function")
+if not result then result = metaLib.overflow("0xoffset", "function")
 
 if not result then exit("Program ended")
 if typeof(result) == "shell" then
